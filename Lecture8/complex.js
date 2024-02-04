92253fc618967d473235aaa591e49de7df341b10
Sanket Singh <singhsanket143@gmail.com>
2024-02-04 12:28:59 +0530

    Added changes

---
+++ Lecture8/complex.js
@@ -0,0 +1,22 @@
+class Complex {
+    constructor(real, imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    add(num) {
+        return `${this.real + num.real} + i${this.imag + num.imag}`
+    }
+    
+
+    multiply(num) {
+        return `${this.real*num.real - this.imag*num.imag} + i ${this.real*num.imag + this.imag*num.real}`
+    }
+}
+
+const c1 = new Complex(3, 5);
+const c2 = new Complex(4, 10);
+
+console.log(c1.multiply(c2));
+
+// (3 + 5i) + (4 + 6i) => (7 + 11i)
